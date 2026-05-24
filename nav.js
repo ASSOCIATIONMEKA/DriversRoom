8a53ceecaf19a30402bc9c7296a12fb00b5159e7
Association MEKA <meka@estaca.eu>
2026-05-24 22:01:38 +0200

    Update nav.js

--- nav.js
+++ nav.js
@@ -12,8 +12,8 @@ document.addEventListener("DOMContentLoaded", function() {
           <div class="dropdown">
             <a href="index.html#competitions" class="dropbtn">NOS COMPÉTITIONS ▾</a>
             <div class="dropdown-content">
-              <a href="login.html">🟢 EstaCup S10 (Nouveau)</a>
-              <a href="s9_archive.html">⚪ EstaCup S9 (Archives)</a>
+              <a href="login.html">🟢 EstaCup S10</a>
+              <a href="s9_archive.html">⚪ EstaCup S9</a>
             </div>
           </div>
           
