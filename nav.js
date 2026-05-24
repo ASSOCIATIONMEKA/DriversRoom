15d57769bb43f3b4a362622dd0a1b54d86804883
Association MEKA <meka@estaca.eu>
2026-05-24 14:45:40 +0200

    Update nav.js

--- nav.js
+++ nav.js
@@ -10,15 +10,17 @@ document.addEventListener("DOMContentLoaded", function() {
           <div class="dropdown">
             <a href="index.html#competitions" class="dropbtn">NOS COMPÉTITIONS ▾</a>
             <div class="dropdown-content">
-              <a href="login.html">🟢 EstaCup S10</a>
-              <a href="s9_archive.html">⚪ EstaCup S9</a>
+              <a href="login.html">🟢 EstaCup S10 (Nouveau)</a>
+              <a href="s9_archive.html">⚪ EstaCup S9 (Archives)</a>
             </div>
           </div>
           <a href="esport.html">ÉQUIPE ESPORT</a>
           <a href="https://discord.gg/jB6yDhQFyw" target="_blank">DISCORD</a>
           <a href="https://twitch.tv/asso_meka" target="_blank">TWITCH</a>
         </div>
+        <a href="login.html" class="nav-btn-login">CONNEXION</a>
     </nav>`;
-// On insère cette navbar juste après l'ouverture de la balise <body>
+
+    // On insère cette navbar juste après l'ouverture de la balise <body>
     document.body.insertAdjacentHTML('afterbegin', navbarHTML);
 });
